2ada43ebf2215e6e3f584e7496d8aeaaddd8b8e3
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    commited changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix to solve inverted matrix repeatedly.
+## 
 
-## Write a short comment describing this function
-
+##created a invertible matrix and chaches it.
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInverse <- NULL
+  set <- function(y) {
+    x <<- y
+    cachedInverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) cachedInverse <<- inverse
+  getInverse <- function() cachedInverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## takes the matrix if not inverted then inverts it from cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invFun <- x$getInverse()
+  if(!is.null(invFun)) {
+    message("getting cached data")
+    return(invFun)
+  }
+  data <- x$get()
+  invFun <- solve(data, ...)
+  x$setInverse(invFun)
+  invFun
 }
